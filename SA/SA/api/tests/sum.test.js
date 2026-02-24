d4b2f8f75bdb161ed4b078c2fb2e55683de78883
Emilie Pessoa Rodrigues <emiliepessoarodrigues@gmail.com>
2026-02-23 22:35:10 -0300

    Teste com toEqual e not.toEqual

--- SA/SA/api/tests/sum.test.js
+++ SA/SA/api/tests/sum.test.js
@@ -26,3 +26,40 @@ describe("The Teste1", () => {
     test("Teste seu nome", () => expect(teste1.seuNome).toBe("seuNome"));
     test("Teste sua idade", () => expect(teste1.suaIdade).toBe("17"));
 });
+
+const produtoA = {
+    nome: "Caderno",
+    preco: 15.90
+}
+
+const produtoB = {
+    nome: "Caderno",
+    preco: 15.90
+}
+
+describe("TesteComparacaoIgual", () => {
+
+    test('Produtos com mesmo valor', () => {
+        expect(produtoA).toEqual(produtoB)
+    });
+
+})
+
+
+const produtoC = {
+    nome: "Caderno",
+    preco: 15.90
+}
+
+const produtoD = {
+    nome: "Caderno",
+    preco: 18.00
+}
+
+describe("TesteComparacaoDiferente", () => {
+
+    test('Produtos com preço diferente', () => {
+        expect(produtoC).not.toEqual(produtoD)
+    });
+
+})
