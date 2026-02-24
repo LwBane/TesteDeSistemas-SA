0189c40fc99c5c012ed9dcfcfd2cfbddbf981cd5
Emilie Pessoa Rodrigues <emiliepessoarodrigues@gmail.com>
2026-02-23 22:26:10 -0300

    Aula 01 - Introdução ao jest no backend

---
+++ SA/SA/api/tests/sum.test.js
@@ -0,0 +1,28 @@
+test('soma de dois mais dois', () => {
+    expect(2+2).toBe(4); // esse toBe é o valor que ele ta recebendo 
+})
+
+const can = {
+    name: 'pamplemousse',
+    ounces: 12,
+};
+
+describe('the can', () => {
+    test('has 12 ounces', () => {
+        expect(can.ounces).toBe(12);
+    });
+
+    test('has a sophisticated name', () => {
+        expect(can.name).toBe('pamplemousse');
+    });
+}); 
+
+const teste1 = {
+    seuNome: 'seuNome',
+    suaIdade: '17'
+}
+
+describe("The Teste1", () => {
+    test("Teste seu nome", () => expect(teste1.seuNome).toBe("seuNome"));
+    test("Teste sua idade", () => expect(teste1.suaIdade).toBe("17"));
+});
